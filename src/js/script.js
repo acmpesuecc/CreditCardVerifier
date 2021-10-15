f44e30d41cc91b6d900d9e2236210e71dfc64e44
saisudhirs <70623429+saisudhirs@users.noreply.github.com>
2021-10-15 21:58:30 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,12 @@ const scheme_validators = [
     regex: /^3(4|7)[0-9]{13}/,
   },
   {
-    name: 'Mastercard',
+    name: 'MasterCard 5-series',
     regex: /^5[1-5]{1}[0-9]{14}/,
+  },
+  {
+    name: 'MasterCard 2-series',
+    regex: /^2[[2-7]{1}[0-9]{14}/,
   },
   {
     name: 'Visa',
@@ -17,6 +21,14 @@ const scheme_validators = [
     name: 'JCB',
     regex: /^35[2-9]{1}[0-9]{13}/,
   },
+  {
+    name: 'Diners Club'
+    regex: /^3(0|6|8)[0-9]{13}/,
+  },
+  {
+    name: 'RuPay'
+    regex: /^6(0|5)[0-9]{14}/,
+  }
 ];
 
 function assign_scheme(number) {
